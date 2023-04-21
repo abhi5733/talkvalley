cae19324c7fbb0a6fbbf7825ebb6063033f35070
abhi5733 <abhijeetkale5733@gmail.com>
2023-04-22 01:52:40 +0530

    add

--- model/userModel.js
+++ model/userModel.js
@@ -9,6 +9,8 @@ description:String,
 imageUrl:String
 })
 
+userSchema.index({ name: "text", primaryText: "text", headline: "text", description: "text" })
+
 const userModel = mongoose.model("user",userSchema)
 
 module.exports = {userModel}
